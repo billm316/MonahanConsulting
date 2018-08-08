4ce8865c016a1d8e9b909f29736a49fe6ceb2c40
william monahan <william.f.monahan@gmail.com>
2018-08-08 14:39:25 -0400

    Initial Version

--- MonahanConsulting/Pages/About.cshtml.cs
+++ MonahanConsulting/Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace MonahanConsulting.Pages
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            //Message = "Your application description page.";
         }
     }
 }
